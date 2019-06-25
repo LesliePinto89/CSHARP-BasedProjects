b16cf94324c0ca91fcc51cea4d8ba984666686f3
LPinto <34793298+LesliePinto89@users.noreply.github.com>
2019-06-25 02:09:58 +0100

    Create Problem #214 (Solved).cs
    Solved coding challenge #214 from Stripe

---
+++ src/Coding Challenges/Stripe/Problem #214 (Solved).cs
@@ -0,0 +1,52 @@
+    /*Given an integer n, return the length of the longest consecutive run of 
+        * 1s in its binary representation.
+        */
+        public void StripeChallenge(int input)
+        {
+            string previousLongest = " ";
+            string findLongest = "";
+            string convertToBinary = Convert.ToString(input, 2);
+
+            for (int i = 0; i <= convertToBinary.Length; i++)
+            {
+                //Keep track of types 1 was encounter in a consecutive sequence each attempt
+                int numberOfOnesCount = findLongest.Count(x => x == '1');
+                int numberOfAlternativeOnesCount = previousLongest.Count(x => x == '1');
+
+                if (i == convertToBinary.Length)
+                {
+                    if (numberOfOnesCount > numberOfAlternativeOnesCount)
+                    {
+                        Console.WriteLine($"Longest number of 1's is {findLongest}");
+                        break;
+                    }
+                    else 
+                    {
+                        Console.WriteLine($"Longest number of 1's is {previousLongest}");
+                        break;
+                    }
+                }
+
+                if (convertToBinary[i] == '1')
+                {
+                    findLongest += convertToBinary[i];
+                }
+
+                else
+                {
+                    //Used only when latest series of 1's are higher than last instance
+                    switch (numberOfOnesCount > numberOfAlternativeOnesCount)
+                    {
+                        case true:
+                            previousLongest = findLongest;
+                            findLongest = "";
+                            break;
+                        case false: continue;
+                        default:
+                            previousLongest = findLongest;
+                            findLongest = "";
+                            break;
+                    }
+                }
+            }
+        }
