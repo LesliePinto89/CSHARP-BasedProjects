a3f6ed55e4133e89475cd21346944db5028c0190
LPinto <34793298+LesliePinto89@users.noreply.github.com>
2019-08-22 19:03:36 +0100

    Refactored source code
    Refactored source code

--- src/Personal Challenges/Three Unit Conversion Dictionary/Program.cs
+++ src/Personal Challenges/Three Unit Conversion Dictionary/Program.cs
@@ -1,66 +1,66 @@
-      /// <summary>
+        private static List<string> upToTwenty;
+        private static List<string> baseTenDoubleHigher;
+        private static string sample;
+        
+		/// <summary>
         /// Finds the natural language equivalent of its numerical
         /// counterpart written in a string. e.g. 132 would return one hundred and thirty two
         /// </summary>
         /// <param name="sample">A string formated numeral value</param>
-        /// <returns></returns>
-        public static string ThreeUnitWordDictionary(string sample)
+        /// <returns>Number to natural language version</returns>
+        public static string ThreeUnitWordDictionary(string data)
         {
+            sample = data;
             if (sample == "0" || sample.Length == 0 || sample.Length > 3) 
                 return null;
             
             int unitOne = Int32.Parse(sample[0].ToString());
             string word = "";
-            List<string> upToTwenty = new List<string> {
+            upToTwenty = new List<string> {
                 "one","two","three","four","five","six","seven","eight",
                 "nine","ten","eleven","twelve","threeteen","fourteen","fiveteen","sixteen",
                 "seventeen","eightteen","nineteen"
             };        
-            List<string> baseTenDoubleHigher = new List<string> {
+            baseTenDoubleHigher = new List<string> {
                 "twenty","thirty","fourty","fifty","sixty","seventy",
                 "eighty","ninety",
             };
 
-            //e.g. 1,2,3
-            if (sample.Length == 1) 
-            word = upToTwenty.Find(x => unitOne-1 == upToTwenty.IndexOf(x));
+            if (sample.Length == 1)  //e.g. 1,2,3
+                word = upToTwenty.Find(x => unitOne-1 == upToTwenty.IndexOf(x));
             
             else if (sample.Length == 2)
-            {
-                //e.g. 11,13
-                int unitTwo = Int32.Parse(sample[0].ToString()+ sample[1].ToString());
-                word = upToTwenty.Find(x => unitTwo == upToTwenty.IndexOf(x));
-                if (word == null) 
-                {
-                    //e.g. 20,30
-                    unitTwo = Int32.Parse(sample[1].ToString());
-                    word = baseTenDoubleHigher.Find(x => unitOne - 2 == baseTenDoubleHigher.IndexOf(x));
-
-                    //e.g. adds one unit to make 31,42
-                    string last = upToTwenty.Find(x => unitTwo-1 == upToTwenty.IndexOf(x));
-                    word += " " + last;
-                }
-            }
-
+                word = HandleDouble(unitOne);
+            
             else if (sample.Length == 3) //e.g.147
-            {
-                int unitTwo = Int32.Parse(sample[1].ToString() + sample[2].ToString());
-                int unitThree = Int32.Parse(sample[1].ToString());
-
+            {         
                 //e.g. 1 in 1 hundred
                 word = upToTwenty.Find(x => unitOne-1 == upToTwenty.IndexOf(x));
                 word += " hundred and ";
-  
-                //same as previous if case
-                string joinWord = upToTwenty.Find(x => unitTwo-1 == upToTwenty.IndexOf(x));
-                if (joinWord == null)
-                {
-                    joinWord = baseTenDoubleHigher.Find(x => unitThree - 2 == baseTenDoubleHigher.IndexOf(x));
-                    string last = upToTwenty.Find(x => unitThree == upToTwenty.IndexOf(x));
-                    word += joinWord + " " + last;
-                }
-                else
-                    word += joinWord;
+                sample = sample.Remove(0, 1);
+                word += HandleDouble(unitOne);
             }
             return word;
+        }
+
+        /// <summary>
+        /// Handle string value from 10 to 19 and from 20 to 99
+        /// </summary>
+        /// <param name="unitOne">Hundreth number</param>
+        /// <returns></returns>
+        public static string HandleDouble(int unitOne) {
+            //e.g. 11,13
+            int unitTwo = Int32.Parse(sample[0].ToString() + sample[1].ToString());
+            string temp = upToTwenty.Find(x => unitTwo-1 == upToTwenty.IndexOf(x));
+            if (temp == null)
+            {
+                //e.g. 20,30
+                unitTwo = Int32.Parse(sample[1].ToString());
+                temp = baseTenDoubleHigher.Find(x => unitOne - 2 == baseTenDoubleHigher.IndexOf(x));
+
+                //e.g. adds one unit to make 31,42
+                string last = upToTwenty.Find(x => unitTwo - 1 == upToTwenty.IndexOf(x));
+                temp += " " + last;
+            }
+            return temp;
         }
